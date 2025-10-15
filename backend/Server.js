bdb9fbdd53d7b2de6d8cfe2a420d407710fc2387
Amanprajapati145 <ak8292333@gmail.com>
2025-10-15 12:37:33 +0530

    fileupdated

--- backend/Server.js
+++ backend/Server.js
@@ -1,3 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected successfully"))
+  .catch((err) => console.log("❌ MongoDB connection error:", err));
+
 // Step-1: Required modules
 const express = require("express");
 const mongoose = require("mongoose");
